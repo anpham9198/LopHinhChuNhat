dcd5f67dfe890c8eb137dd05435fa47d56f9c08d
anpham9198 <anpham9198@gmail.com>
2024-11-24 16:59:03 +0700

    Add project files.

---
+++ LopHinhChuNhat/Program.cs
@@ -0,0 +1,16 @@
+﻿using LopHinhChuNhat;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chieu rong");
+        float width= float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap chieu dai");
+        float height= float.Parse(Console.ReadLine());
+        Rectangle rectangle= new Rectangle(width,height);
+        rectangle.Show();
+        Console.WriteLine("Gia tri gap doi cua dien tich la {0}",rectangle.GetArea() * 2);
+
+    }
+}
---
+++ LopHinhChuNhat/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿namespace LopHinhChuNhat
+{
+    public class Rectangle
+    {
+        float width = 0;
+
+        float height = 0;
+
+        public Rectangle(float width, float height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public float GetArea()
+        {
+            return width * height;
+        }
+
+        public float GetPrimeter()
+        {
+            return (width + height) * 2;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Chieu dai va chieu rong la {0}{1}", width, height);
+            Console.WriteLine("Dien tich la {0}", GetArea());
+            Console.WriteLine("Chu vi la {0}", GetPrimeter());
+        }
+    }
+}
